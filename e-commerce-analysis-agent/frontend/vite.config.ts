f2f4536758f19a62f01151e4b3052b95c6344584
Ravi Kumar Srirangam <ravisk@gmail.com>
2025-04-22 09:28:15 +0530

    feat: env file checkin

--- e-commerce-analysis-agent/frontend/vite.config.ts
+++ e-commerce-analysis-agent/frontend/vite.config.ts
@@ -8,10 +8,7 @@ export default defineConfig({
   preview: {
    port: 3000,
    strictPort: true,
-   allowedHosts: ["wealth-manager-agent-v2.1s6emm0ktx8z.us-east.codeengine.appdomain.cloud", 
-                  "dsce-test-ce-wtx-wealth-manager-single-agent-ui.1pde6bclcwl3.us-south.codeengine.appdomain.cloud",
-                  "dsce-prod-ce-wtx-wealth-manager-single-agent-ui.1op8ay1afyb7.us-south.codeengine.appdomain.cloud",
-                  "58691dd33e.dsceapp.buildlab.cloud",
+   allowedHosts: [
                   "ecomfrontend.1uijsxnqve2c.us-south.codeengine.appdomain.cloud"
                  ]
   },
